9a531d012fdbc1c51e24f958a0ad0c86d0629c68
surajmendhe5573 <surajmendhe470@gmail.com>
2025-05-25 12:03:36 +0530

    auth api

---
+++ server.js
@@ -0,0 +1,20 @@
+const express= require('express');
+const app= express();
+require('dotenv').config();
+
+app.use(express.json());
+
+const port= process.env.PORT || 5000;
+
+app.get('/', (req, res)=>{
+    res.send('Jai Shree Ram');
+})
+
+require('./config/db');
+
+app.use('/api/users', require('./routes/user'));
+
+app.listen(port, ()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+    
+})
